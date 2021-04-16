9e980da2b74f8ad2802cda082b7e753ce32826dc
garberadamc <agarber@ucsb.edu>
2021-04-15 20:30:19 -0700

    Initial commit, site incomplete

---
+++ create-distill-site.R
@@ -0,0 +1,12 @@
+
+# install packages from github using {remotes}
+
+remotes::install_github("rstudio/distill")
+remotes::install_github("rstudio/rmarkdown")
+
+library(postcards)
+library(distill)
+
+create_website(dir = ".", title = "Adam Garber", gh_pages = TRUE)
+
+create_postcard("file = landing-page.Rmd")
